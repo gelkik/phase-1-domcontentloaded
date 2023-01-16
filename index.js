929d53835282f040270a55b13f5eb5d02497141f
George Elkik <gelkik@gmail.com>
2023-01-16 16:31:42 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    const pam = document.querySelector('#text');
+    pam.textContent = 'This is really cool!';
+})
